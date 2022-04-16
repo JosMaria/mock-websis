8e3ac39ec4e015590f008ffcbc9bd6c537dc973b
Jose Maria <josmariaguilar@gmail.com>
2022-04-16 13:32:29 -0400

    started project modk websis

---
+++ src/main/java/org/genesiscode/mockwebsis/MockWebsisApplication.java
@@ -0,0 +1,13 @@
+package org.genesiscode.mockwebsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockWebsisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockWebsisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/genesiscode/mockwebsis/MockWebsisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.genesiscode.mockwebsis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockWebsisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
