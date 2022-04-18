e8b7a533e27c1e821ce02f79992514bd625af07a
Jose Maria <josmariaguilar@gmail.com>
2022-04-18 14:37:26 -0400

    properties of swagger in .properties file

--- src/main/java/org/genesiscode/mockwebsis/configuration/OpenApi.java
+++ src/main/java/org/genesiscode/mockwebsis/configuration/OpenApi.java
@@ -1,38 +1,66 @@
 package org.genesiscode.mockwebsis.configuration;
 
-
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:api-info.properties")
 public class OpenApi {
+
+    @Value("${app.info.contact.name}")
+    private String contactName;
+
+    @Value("${app.info.contact.email}")
+    private String contactEmail;
+
+    @Value("${app.info.license.name}")
+    private String nameLicense;
+
+    @Value("${app.info.license.url}")
+    private String urlLicense;
+
+    @Value("${app.info.title}")
+    private String title;
+
+    @Value("${app.info.description}")
+    private String description;
+
+    @Value("${app.info.version}")
+    private String version;
+
+    @Value("${app.info.external.documentation.description}")
+    private String externalDescription;
+
+    @Value("${app.info.external.documentation.url}")
+    private String externalUrl;
 
     @Bean
     public OpenAPI springShopOpenAPI() {
         Contact contact = new Contact()
-                .name("Michisoft")
-                .email("michosft@gmail.com");
-
+                .name(contactName)
+                .email(contactEmail);
 
         License license = new License()
-                .name("Apache 2.0")
-                .url("http://springdoc.org");
+                .name(nameLicense)
+                .url(urlLicense);
 
         Info info = new Info()
                 .contact(contact)
-                .title("Websis System")
-                .description("Este sistema nos proporcionara informacion sobre los docentes")
-                .version("v1")
+                .title(title)
+                .description(description)
+                .version(version)
                 .license(license);
 
         ExternalDocumentation externalDocumentation = new ExternalDocumentation()
-                .description("SpringShop Wiki Documentation")
-                .url("https://springshop.wiki.github.org/docs");
+                .description(externalDescription)
+                .url(externalUrl);
 
         return new OpenAPI()
                 .info(info)
