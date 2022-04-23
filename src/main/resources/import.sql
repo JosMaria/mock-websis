54804685766d4bb329bce0cbed31055323242efa
Jose Maria <josmariaguilar@gmail.com>
2022-04-23 08:03:03 -0400

    Data load to teachers table

---
+++ src/main/resources/import.sql
@@ -0,0 +1,45 @@
+-- INSERT TEACHERS WITH LEVEL OF STUDIES F
+INSERT INTO teachers (id, name, last_name, email, password) VALUES (1, 'HENRY FRANK', 'VILLARROEL TAPIA', 'henryfrank@gmail.com', 'henryfrank17');
+INSERT INTO teachers (id, name, last_name, email, password) VALUES (2, 'INDIRA', 'CAMACHO DEL CASTILLO', 'indira@gmail.com', 'indira17');
+INSERT INTO teachers (id, name, last_name, email, password) VALUES (3, 'ROSEMARY', 'TORRICO BASCOPE', 'rosemary@gmail.com', 'rosemary17');
+INSERT INTO teachers (id, name, last_name, email, password) VALUES (4, 'CARMEN ROSA', 'GARCIA PEREZ', 'carmenrosa@gmail.com', 'carmen17');
+INSERT INTO teachers (id, name, last_name, email, password) VALUES (5, 'ERIKA PATRICIA', 'RODRIGUEZ BILBAO', 'erikapatricia@gmail.com', 'erika17');
+INSERT INTO teachers (id, name, last_name, email, password) VALUES (6, 'JORGE WALTER', 'ORELLANA ARAOZ', 'jorgewalter@gmail.com', 'jorge17');
+INSERT INTO teachers (id, name, last_name, email, password) VALUES (7, 'JUAN RUBEN', 'GARCIA MOLINA', 'juanruben@gmail.com', 'juan17');
+INSERT INTO teachers (id, name, last_name, email, password) VALUES (8, 'EMIR FELIX', 'VARGAS PEREDO', 'emirfelix@gmail.com', 'emir17');
+INSERT INTO teachers (id, name, last_name, email, password) VALUES (9, 'YONY RICHARD', 'MONTOYA BURGOS', 'yonyrichard@gmail.com', 'yony17');
+
+--  (5, 'ERIKA PATRICIA', 'RODRIGUEZ BILBAO', 'erikapatricia@gmail.com', 'erika17'),
+--  (5, 'ERIKA PATRICIA', 'RODRIGUEZ BILBAO', 'erikapatricia@gmail.com', 'erika17'),
+--  (5, 'ERIKA PATRICIA', 'RODRIGUEZ BILBAO', 'erikapatricia@gmail.com', 'erika17'),
+--  (5, 'ERIKA PATRICIA', 'RODRIGUEZ BILBAO', 'erikapatricia@gmail.com', 'erika17'),
+--  (5, 'ERIKA PATRICIA', 'RODRIGUEZ BILBAO', 'erikapatricia@gmail.com', 'erika17'),
+--  (5, 'ERIKA PATRICIA', 'RODRIGUEZ BILBAO', 'erikapatricia@gmail.com', 'erika17'),
+--  (5, 'ERIKA PATRICIA', 'RODRIGUEZ BILBAO', 'erikapatricia@gmail.com', 'erika17'),
+--  (5, 'ERIKA PATRICIA', 'RODRIGUEZ BILBAO', 'erikapatricia@gmail.com', 'erika17'),
+--  (5, 'ERIKA PATRICIA', 'RODRIGUEZ BILBAO', 'erikapatricia@gmail.com', 'erika17'),
+--  (5, 'ERIKA PATRICIA', 'RODRIGUEZ BILBAO', 'erikapatricia@gmail.com', 'erika17'),
+
+--     FLORES VILLARROEL CORINA
+--     BLANCO COCA LETICIA
+--     ESCALERA FERNANDEZ DAVID
+--     MONTECINOS CHOQUE MARCO ANTONIO
+--     ACHA PEREZ SAMUEL
+--     VARGAS ANTEZANA ADEMAR MARCELO
+--     GRILO SALVATIERRA MARIA ESTELA
+--     ROMERO RODRIGUEZ PATRICIA
+--     VILLARROEL NOVILLO JIMMY
+--     AYOROA CARDOZO JOSE RICHARD
+--     JALDIN ROSALES K. ROLANDO
+--     FIORILO LOZADA AMERICO
+--     ANTEZANA CAMACHO MARCELO
+--     GUZMAN ORELLANA GONZALO ENRIQUE
+--     MONTANO QUIROGA VICTOR HUGO
+--     COSTAS JAUREGUI VLADIMIR ABEL
+--     CALANCHA NAVIA BORIS
+--     QUIROZ CHAVEZ ABDON
+--     ARANIBAR LA FUENTE LIGIA
+--     CHOQUE FLORES ALEX D'ANCHGELO
+-- LIMA VACAFLOR TITO ANIBAL
+-- MEJIA URQUIETA VICTOR RAMIRO
+-- COSIO PAPADOPOLIS CARLOS JAVIER
